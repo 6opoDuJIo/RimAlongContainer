68d2a5f292d12b849615d076b271f12d04057569
gleb.avietisov <gleb.avietisov@envisiontec.de>
2018-10-01 18:00:26 +0300

    Clean gh-pages branch

---
+++ RimAlongDIContainer/SessionInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RimAlongDIContainer
+{
+    public interface SessionInfo
+    {
+        int GameTickCount { get; }
+        int NextSyncTickID { get; }
+		int SyncRoundCount { get; }
+		int SyncRoundDurationInMiliseconds { get; }
+		int SessionGenericRandSeed { get; }
+		string SessionMapSeed { get; }
+		IEnumerable<int> InitialSessionPawnSeeds { get; }
+        IEnumerable<PlayerInfo> Players { get; }
+        SessionState State { get; }
+		IEnumerable<KeyValuePair<PlayerInfo, string>> GetChatHistory();
+    }
+}
