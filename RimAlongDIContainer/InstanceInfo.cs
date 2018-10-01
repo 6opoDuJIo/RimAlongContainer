37a23f5198c615082f23e86974960b33481a4802
gleb.avietisov <gleb.avietisov@envisiontec.de>
2018-10-01 20:11:00 +0300

    Added version and instance info

---
+++ RimAlongDIContainer/InstanceInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RimAlongDIContainer
+{
+    public enum NetworkingModes
+    {
+        GameNetworkingSocketsOpensource,
+        GameNetworkingSocketsSteam,
+        Custom,
+        None,
+        Mocked
+    }
+
+    public interface InstanceInfo
+    {
+        string RimAlongVersionID { get; }
+        string ProtocolVersionID { get; }
+        NetworkingModes NetworkingMode { get; }
+        SessionInfo CurrentSession { get; }
+    }
+}
